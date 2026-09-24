2223f8f9c207a2bb4feb1716a9c4faad61558e96
AnanyaPandey-Dev <ananyapandey.dev.in@gmail.com>
2026-09-24 11:15:18 +0530

    Fix Google Translate stylesheet CSP and update documentation

--- next.config.mjs
+++ next.config.mjs
@@ -75,7 +75,7 @@ const nextConfig = {
             value: [
               "default-src 'self'",
               "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://apis.google.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://translate.google.com https://translate.googleapis.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://translate.googleapis.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://translate.googleapis.com https://www.gstatic.com",
               "img-src 'self' data: blob: https: http: https://translate.googleapis.com https://www.gstatic.com",
               "font-src 'self' https://fonts.gstatic.com",
               "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://accounts.google.com https://oauth2.googleapis.com https://www.googleapis.com https://translate.googleapis.com https://*.vercel.app https://*.upstash.io",
